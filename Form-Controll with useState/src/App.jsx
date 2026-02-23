c7b368d5480e59fb8238390a6f83c534481e872c
BYIRINGIRO Sam Franco <ancosamfranco250@gmail.com>
2026-02-23 13:14:16 +0200

    Add files via upload

---
+++ Form-Controll with useState/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import StateHook from './StateHook'
+import Form from './Form'
+import PersonData from './PersonData'
+
+
+function App() {
+  return (
+    <div>
+      <PersonData/>
+    </div>
+  )
+}
+
+export default App
